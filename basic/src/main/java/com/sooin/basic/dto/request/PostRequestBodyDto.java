4dad40e653d6473797ca8a478373f7556b41ad1b
Sooin <sooin0458@gmail.com>
2023-08-01 15:19:49 +0900

    2023.08.01 15:30

--- basic/src/main/java/com/sooin/basic/dto/request/PostRequestBodyDto.java
+++ basic/src/main/java/com/sooin/basic/dto/request/PostRequestBodyDto.java
@@ -1,25 +1,19 @@
 package com.sooin.basic.dto.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class PostRequestBodyDto {
+  @NotBlank
   private String name;
-  private int age;
-
-  public PostRequestBodyDto () {}
-
-  public void setAge(int age) {
-    this.age = age;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public String getName() {
-    return name;
-  }
+  @NotNull
+  private Integer age;
   
 }
