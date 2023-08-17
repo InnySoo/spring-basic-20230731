a13dea2c2b6799139d6fd4e18560c32865752f00
Sooin <sooin0458@gmail.com>
2023-08-17 11:11:41 +0900

    2023.08.17 11:10

--- basic/src/main/java/com/sooin/basic/controller/MainController.java
+++ basic/src/main/java/com/sooin/basic/controller/MainController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -121,6 +122,13 @@ public class MainController {
   ) {
     String subject = jwtProvider.validate(jwt);
     return subject;
+  }
+
+  @PostMapping("/principle")
+  public String principle (
+    @AuthenticationPrincipal String subject
+  ) {
+    return "토큰에 포함된 subject는 " + subject + "입니다.";
   }
 
 }
